28aacd9d8d200fedbc8d6e8c08565557e7c35a47
Jaime de Venecia <deveneciaj@jdv-plus-2022.lan>
2023-10-30 00:30:52 -0700

    Add optimized solution for validParentheses

--- easy/2-validParentheses.ts
+++ easy/2-validParentheses.ts
@@ -41,3 +41,36 @@ const isValidTS = (s: string): boolean => {
 
   return stack.length === 0;
 };
+
+/*
+OPTIMIZED SOLUTION
+Runtime: 42ms (Beats 99.50% of users with TypeScript)
+Memory: 42.54 MB (Beats 98.10% of users with TypeScript)
+*/
+
+interface BracketDict {
+  '(': string;
+  '{': string;
+  '[': string;
+}
+
+const isValidOptimized = (s: string): boolean => {
+  const brackets: BracketDict = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+
+  const stack: string[] = [];
+
+  for (let i = 0; i < s.length; i++) {
+    let curr: string = s[i];
+
+    if (brackets.hasOwnProperty(curr)) stack.push(curr);
+    else {
+      if (!stack.length || brackets[stack.pop()!] !== curr) return false;
+    }
+  }
+
+  return stack.length === 0;
+};
