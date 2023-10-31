b30ac09c04caac1bfb4200db538bc59fdb16c143
Jaime de Venecia <deveneciaj@jdv-plus-2022.lan>
2023-10-31 09:41:36 -0700

    Add solutions for 6

---
+++ easy/6-invertTree.ts
@@ -0,0 +1,42 @@
+/*
+
+226. Invert Binary Tree
+
+Given the root of a binary tree, invert the tree, and return its root.
+
+Example 1:
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Example 2:
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+Example 3:
+Input: root = []
+Output: []
+
+*/
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = !val ? 0 : val;
+    this.left = !left ? null : left;
+    this.right = !right ? null : right;
+  }
+}
+
+const invertTreeTS = (root: TreeNode | null): TreeNode | null => {
+  if (root) {
+    [root.left, root.right] = [root.right, root.left];
+
+    if (root.left) invertTreeTS(root.left);
+    if (root.right) invertTreeTS(root.right);
+  }
+
+  return root;
+};
